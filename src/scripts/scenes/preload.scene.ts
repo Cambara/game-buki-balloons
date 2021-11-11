ee0326b88ae9b431038f9e9e6e8549fb6fa9a869
Tiago Cambara <tiago.cambara21@gmail.com>
2021-11-11 21:23:58 +0000

    feat: add the ana's sprintes

--- src/scripts/scenes/preload.scene.ts
+++ src/scripts/scenes/preload.scene.ts
@@ -8,11 +8,12 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.image('letter-icon', 'assets/items/letter/letter.jpeg')
     this.load.atlas('faune', 'assets/avatars/faune/texture.png', 'assets/avatars/faune/texture.json')
     this.load.atlas('balloon', 'assets/items/balloon/texture.png', 'assets/items/balloon/texture.json')
+    this.load.atlas('ana', 'assets/npcs/ana/texture.png', 'assets/npcs/ana/texture.json')
     this.load.audio('balloon-blowup', 'assets/items/balloon/blowup.mp3')
     this.load.image('buki_avatar', 'assets/avatars/buki/avatar.png')
     this.load.image('ana_avatar', 'assets/npcs/ana/avatar.png')
   }
-
+  
   create() {
     this.scene.start('StartMenuScene')
   }
