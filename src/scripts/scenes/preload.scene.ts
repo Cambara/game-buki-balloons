df6072722d7ea82dbd6ec4f06f61ee4be55d98bc
Tiago Cambara <tiago.cambara21@gmail.com>
2021-11-11 21:56:32 +0000

    feat: add wind sprite

--- src/scripts/scenes/preload.scene.ts
+++ src/scripts/scenes/preload.scene.ts
@@ -9,6 +9,7 @@ export default class PreloadScene extends Phaser.Scene {
     this.load.atlas('faune', 'assets/avatars/faune/texture.png', 'assets/avatars/faune/texture.json')
     this.load.atlas('balloon', 'assets/items/balloon/texture.png', 'assets/items/balloon/texture.json')
     this.load.atlas('ana', 'assets/npcs/ana/texture.png', 'assets/npcs/ana/texture.json')
+    this.load.atlas('wind', 'assets/npcs/wind/texture.png', 'assets/npcs/wind/texture.json')
     this.load.audio('balloon-blowup', 'assets/items/balloon/blowup.mp3')
     this.load.image('buki_avatar', 'assets/avatars/buki/avatar.png')
     this.load.image('ana_avatar', 'assets/npcs/ana/avatar.png')
